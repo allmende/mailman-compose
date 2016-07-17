36c8a11c310497cd3e0151fe4bd4d9f4e09cc3e3
Simon Hanna <simon.hanna@jesus.de>
2016-07-17 18:32:18 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Docker compose files for GNU Mailman
+
+This is a collection of docker compose files that can be used to try out GNU
+Mailman.
+
+Run the specific files using: `docker-compose -f <file_name> up --build`
+
+Refer to the options available for the images in their respective repositories:
+
+* Mailman: https://github.com/simonsmiley/mailman-docker/
+* Postorius: https://github.com/simonsmiley/postorius-docker/
---
+++ postorius_config/settings.py
@@ -0,0 +1,12 @@
+### You can use this file to overwrite the settings
+### NOTE that this overrides environment variables!
+# MAILMAN_REST_API_URL = 'http://localhost:8001'
+# MAILMAN_REST_API_USER = 'restadmin'
+# MAILMAN_REST_API_PASS = 'restpass'
+# 
+# LOGIN_URL = 'user_login'
+# LOGIN_REDIRECT_URL = 'list_index'
+# LOGOUT_URL = 'user_logout'
+# 
+# DEFAULT_FROM_EMAIL = 'postorius@localhost.local'
+# SERVER_EMAIL = 'root@localhost.local'
